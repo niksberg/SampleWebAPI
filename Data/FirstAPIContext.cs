56f4dda16da6f7ea520278e92cb671648517b270
Nikshep M P <nikhilmpgowda@gmail.com>
2025-07-19 14:25:32 +0530

    Utilizing Entity Framework

---
+++ Data/FirstAPIContext.cs
@@ -0,0 +1,15 @@
+﻿using FirstAPI.Controllers;
+using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FirstAPI.Data
+{
+    public class FirstAPIContext : DbContext
+    {
+        public FirstAPIContext(DbContextOptions<FirstAPIContext> options):base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
